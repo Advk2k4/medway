9edfebcc5f388c17ca04875da72d59503682047f
GAUTAM22042 <gjayakishan@umass.edu>
2025-08-06 14:32:32 +0530

    Updated login,button,sign and input

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,11 +1,11 @@
-const Button = ({ children, variant = "primary", ...props }) => {
+const Button = ({ children, variant = "primary", className= "", ...props }) => {
   const styles = {
     primary: "bg-green-800 text-white shadow-md hover:bg-green-900",
     secondary: "bg-[#E7DCC6] text-green-900 shadow-md hover:bg-[#d6cdbb]",
   };
   return (
     <button
-      className={`px-6 py-2 rounded-md font-semibold ${styles[variant]}`}
+      className={`bg-[#065F2B] text-xl hover:bg-[#054a23] text-[#F7F7F7] font-semibold py-[16px] px-12 rounded-[30px] w-full transition-colors duration-200 ${className}`}
       {...props}
     >
       {children}
